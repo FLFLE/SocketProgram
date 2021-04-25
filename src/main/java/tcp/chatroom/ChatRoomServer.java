ff8127413ff6ff09427e503e99730b63c57e0ec6
FLFLE <1792236446@qq.com>
2021-04-25 15:22:29 +0800

    TCP server

--- src/main/java/tcp/chatroom/ChatRoomServer.java
+++ src/main/java/tcp/chatroom/ChatRoomServer.java
@@ -19,7 +19,7 @@ public class ChatRoomServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 socket = serverSocket.accept();
-                System.out.println("用户:"+socket.getInetAddress().getHostAddress()+"进入聊天室");
+                System.out.println("用户:" + socket.getInetAddress().getHostAddress() + "进入聊天室");
                 System.out.println("--------------------");
                 socketList.add(socket);
                 new Thread(new Server(socket)).start();
@@ -39,11 +39,12 @@ public class ChatRoomServer {
 
         public void run() {
             String message;
-            byte[] buffer = new byte[1024];
+
             InputStream ins;
             OutputStream os;
             try {
                 while (true) {
+                    byte[] buffer = new byte[1024];
                     //接受当前用户发送的消息
                     ins = socket.getInputStream();
                     ins.read(buffer);
@@ -52,7 +53,7 @@ public class ChatRoomServer {
 
                     //将消息转发给所有用户
                     for (Socket s : socketList) {
-                        if (s!=socket) {
+                        if (s != socket) {
                             os = s.getOutputStream();
                             os.write(message.getBytes());
                         }
